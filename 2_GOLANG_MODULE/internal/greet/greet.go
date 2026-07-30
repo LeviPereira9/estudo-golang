16be7eaa00922cc2fa1302303432dba563bac72c
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2026-07-30 16:46:54 -0300

    2 - modulos

---
+++ 2_GOLANG_MODULE/internal/greet/greet.go
@@ -0,0 +1,21 @@
+package greet
+
+import "strings"
+
+// exported functions starts with Capital letter
+// other packages can call it -> p1, p2
+func Hello(name string) string{
+	clean := normalizeName(name)
+
+	return "Hello, " + clean
+}
+
+func normalizeName(name string) string{
+	n := strings.TrimSpace(name)
+	
+	if n == ""{
+		return "Guest"
+	}
+	
+	return strings.ToUpper(n)
+}
